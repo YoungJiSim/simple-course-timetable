1f1f97f5254b753055aa6f29a0b26fde63cbbf62
Young Ji Sim <yj.sim.ca@gmail.com>
2025-12-24 22:45:24 -0800

    Update CSS for course form page

--- courseForm.js
+++ courseForm.js
@@ -10,29 +10,49 @@ function init() {
 function addScheduleInputs() {
   const scheduleHtml = `
           <div class="scheduleSets">
-            <label for="CRN">CRN: </label>
-            <input type="text" class="CRN" name="CRN" />
-            <label for="classType">Class Type: </label>
-            <select name="classType" class="classType">
-              <option value="lecture">Lecture</option>
-              <option value="lab">Lab</option>
-            </select>
-            <label for="day">Day: </label>
-            <select name="day" class="day">
-              <option value="mon">MON</option>
-              <option value="tue">TUE</option>
-              <option value="wed">WED</option>
-              <option value="thu">THU</option>
-              <option value="fri">FRI</option>
-            </select>
-            <label for="startTime">Class Start Time: </label>
-            <input type="time" class="startTime" name="startTime" />
-            <label for="endTime">Class End Time: </label>
-            <input type="time" class="endTime" name="endTime" />
-            <label for="classroom">Classroom: </label>
-            <input type="text" class="classroom" name="classroom" />
+            <div class="inputGroupDivs">
+              <div class="inputDivs">
+                <label for="CRN">CRN: </label>
+                <input type="text" class="CRN" name="CRN" />
+              </div>
+            </div>
+            <div class="inputGroupDivs">
+              <div class="inputDivs">
+                <label for="classroom">Classroom: </label>
+                <input type="text" class="classroom" name="classroom" />
+              </div>
+              <div class="inputDivs">
+                <label for="classType">Class Type: </label>
+                <select name="classType" class="classType">
+                  <option value="lecture">Lecture</option>
+                  <option value="lab">Lab</option>
+                </select>
+              </div>
+            </div>
+            <div class="inputGroupDivs">
+              <div class="inputDivs">
+                <label for="day">Day: </label>
+                <select name="day" class="day">
+                  <option value="mon">MON</option>
+                  <option value="tue">TUE</option>
+                  <option value="wed">WED</option>
+                  <option value="thu">THU</option>
+                  <option value="fri">FRI</option>
+                </select>
+              </div>
+            </div>
+            <div class="inputGroupDivs">
+              <div class="inputDivs">
+                <label for="startTime">Class Start: </label>
+                <input type="time" class="startTime" name="startTime" />
+              </div>
+              <div class="inputDivs">
+                <label for="endTime">Class End: </label>
+                <input type="time" class="endTime" name="endTime" />
+              </div>
+            </div>
             <button type="button" class="deleteScheduleBtns" onClick="deleteSchedule(this)">
-              Delete Schedule
+            Delete Schedule
             </button>
           </div>`;
 
